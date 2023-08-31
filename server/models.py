3427457d07f5e4c79672cbeb347da25ab266f0fc
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-08-31 14:30:48 -0500

    started models

--- server/models.py
+++ server/models.py
@@ -1,3 +1,36 @@
+from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+from sqlalchemy import MetaData
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
+from config import db, bcrypt
+from flask_login import UserMixin
+
 class User(db.Model):
     __tablename__="users"
     id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    email = db.Column(db.String, unique = True)
+    _password_hash = db.Column(db.String)
+
+class Guest(db.Model):
+    __tablename__ = "guests"
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    email = db.Column(db.String, unique = True)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
+
+class Order(db.Model):
+    __tablename__ = "orders"
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+    items = db.Column(db.Array(db.String))
+
